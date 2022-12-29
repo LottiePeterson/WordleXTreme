e7882e41129eafabd10a4bae1744b7af76b44172
Lottie Peterson <LottiePeterson13@gmail.com>
2022-12-29 16:00:42 -0600

    Added .env functionality to keep the token for the bot as an environment variable. Will need to add a .env file in the src/main/resources directory with the token listed in it for local copies. Gotta protect that token!

--- src/main/java/com/lenerdz/WordleXTreme.java
+++ src/main/java/com/lenerdz/WordleXTreme.java
@@ -3,15 +3,19 @@ package com.lenerdz;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import javax.security.auth.login.LoginException;
 
 public class WordleXTreme {
 
     public static void main(String[] args) throws LoginException {
+        
+        Dotenv dotenv = Dotenv.load();
+        dotenv.get("MY_ENV_VAR1");
 
-        JDA bot = JDABuilder.createDefault("")
-                .setActivity(Activity.playing("with your mom"))
+        JDA bot = JDABuilder.createDefault(dotenv.get("TOKEN"))
+                .setActivity(Activity.playing("testy test time"))
                 .build();
 
     }
