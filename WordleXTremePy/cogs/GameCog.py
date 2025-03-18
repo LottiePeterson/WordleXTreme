5ea3ff6b5f8fff9fb8508002729ef3410a404cc4
Lottie Peterson <LottiePeterson13@gmail.com>
2025-03-18 02:02:38 -0500

    Added infrastructure for a subcommand

--- WordleXTremePy/cogs/GameCog.py
+++ WordleXTremePy/cogs/GameCog.py
@@ -3,34 +3,42 @@ from database import db
 class GameCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
-    @commands.command(aliases=['game', 'curr', 'g'])
+
+    @commands.group(aliases=['game', 'curr', 'g'])
     async def _game(self, ctx):
-        guildId = str(ctx.guild.id)
-        query = ("SELECT p.WordleName, gp.PlayerID, g.Name, "
-                "SUM(s.SubScore) AS totalSubScore, "
-                "SUM(s.SuperScore) AS totalSuperScore "
-                "FROM GamesPlayers gp JOIN Games g ON gp.GameID = g.ID "
-                                     "JOIN Scores s ON gp.PlayerID = s.PlayerID AND gp.GameID = s.GameID "
-                                     "JOIN Players p on gp.PlayerID = p.ID "
-                                     "JOIN Guilds gd ON gd.ID = g.GuildID "
-                "WHERE g.Current = 1 AND g.GuildID = %s "
-                "GROUP BY p.WordleName, gp.PlayerID, gp.GameID "
-                "ORDER BY p.WordleName;")
-        summary = db.fetch(query,(guildId))
+        if ctx.invoked_subcommand is None:
+            # await ctx.send("Please use `!score submit` or `!score history`")
         
-        if not summary:
-            await ctx.send("No current games in this server!")
-        else:
-            status = ''
-            for r_idx, row in enumerate(summary):
-                if r_idx == 0:
-                    header = '===== **' + str(row['Name']) + '** =====\n'
-                    status += header
-                status += row['WordleName'] + ': ' + str(row['totalSuperScore']) + ' ' + str(row['totalSubScore']) + '\n'
-            for _ in range(len(header) - 4):
-                status += '='
-            await ctx.send(status)
+            guildId = str(ctx.guild.id)
+            query = ("SELECT p.WordleName, gp.PlayerID, g.Name, "
+                    "SUM(s.SubScore) AS totalSubScore, "
+                    "SUM(s.SuperScore) AS totalSuperScore "
+                    "FROM GamesPlayers gp JOIN Games g ON gp.GameID = g.ID "
+                                        "JOIN Scores s ON gp.PlayerID = s.PlayerID AND gp.GameID = s.GameID "
+                                        "JOIN Players p on gp.PlayerID = p.ID "
+                                        "JOIN Guilds gd ON gd.ID = g.GuildID "
+                    "WHERE g.Current = 1 AND g.GuildID = %s "
+                    "GROUP BY p.WordleName, gp.PlayerID, gp.GameID "
+                    "ORDER BY p.WordleName;")
+            summary = db.fetch(query,(guildId))
+            
+            if not summary:
+                await ctx.send("No current games in this server!")
+            else:
+                status = ''
+                for r_idx, row in enumerate(summary):
+                    if r_idx == 0:
+                        header = '===== **' + str(row['Name']) + '** =====\n'
+                        status += header
+                    status += row['WordleName'] + ': ' + str(row['totalSuperScore']) + ' ' + str(row['totalSubScore']) + '\n'
+                for _ in range(len(header) - 4):
+                    status += '='
+                await ctx.send(status)
+
+    @_game.command(aliases=['new', 'n'])
+    async def _game_new(self, ctx, name):
+        # create a new game with a given name
+        await ctx.send(f"Hey! New game to be made with the name {name}")
 
 async def setup(bot):
     await bot.add_cog(GameCog(bot))
